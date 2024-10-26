a92e40ab61e8080a43820f2b90889d13510e2073
S1rDavid9 <d.akachi@alustudent.com>
2024-10-26 14:53:22 +0000

    top ten

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,31 +1,16 @@
 #!/usr/bin/python3
-"""1-top_ten.py"""
-
+""" top_ten.py """
 import requests
 
 
 def top_ten(subreddit):
-    """Prints the titles of the first 10 hot posts of a subreddit."""
-    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
-    headers = {"User-Agent": "MyRedditApp/0.1 (by u/yourusername)"}
-
-    # Send a GET request to the Reddit API
+    """ prints the titles of the first 10 hot posts listed in a subreddit """
+    url = 'https://www.reddit.com/r/{}/hot.json?limit=10'.format(subreddit)
+    headers = {'User-Agent': 'Mozilla/5.0'}
     response = requests.get(url, headers=headers, allow_redirects=False)
-
-    # Check if the response is not valid (status code != 200)
     if response.status_code != 200:
-        print("None")
+        print(None)
         return
-
-    # Try to extract data
-    try:
-        posts = response.json().get("data", {}).get("children", [])
-        if not posts:
-            print("None")
-            return
-
-        # Print titles of the first 10 hot posts
-        for post in posts[:10]:  # Ensure we only print 10 items
-            print(post.get("data", {}).get("title"))
-    except Exception:
-        print("None")
+    posts = response.json()['data']['children']
+    for post in posts:
+        print(post['data']['title'])
